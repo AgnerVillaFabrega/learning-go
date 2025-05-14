99ad99fe1a04bdeb6c60ce40a09bfea60506c3df
AgnerVillaFabrega <adejesusvilla@unicesar.edu.co>
2025-05-14 17:52:53 -0500

    defeder | condiciones | bucles[rango|valor|infinito] | range | switch

--- main.go
+++ main.go
@@ -1,64 +1,69 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+import "fmt"
 
 func main() {
-	// Numeros
-	// var entero int = 10
-	entero := 10
-	decimal := 3.14
-	suma := entero + int(decimal)
+	// Defer -> ejecuta cuando termine toda la logica asociada a funcion
+	defer fmt.Println("FIN")
 
-	// Textos
-	mensaje := "Hola,"
-	concatenado := mensaje + "Gentleman"
-	enMayusculas := strings.ToUpper(concatenado)
 
-	// Booleanos
-	esVerdadero := true
+	//  Condicionales
+	// Assertive / negative programming
+	edad := 20
 
-	// Arrays y Slices
-	arrayFijo := [3]int{1,2,3}
-	sliceVariable := []int{4,5,6}
-	sliceVariable = append(sliceVariable, 7)
-
-	// Mapas
-	diccionario := map[string]int{
-		"clave1" : 1,
-		"clave2": 2,
+	if edad < 18 {
+		fmt.Println("Eres menor de edad")
+		return
 	}
 
-	// Structs
-	type Persona struct {
-		Nombre string
-		Edad int
+	fmt.Println("Eres mayor de edad")
+
+	// Bucle [Rango]
+	for i := 0; i < 5; i++ {
+		fmt.Printf("Iteracion: %d\n", i)
 	}
 
-	persona := Persona{Nombre: "Agner", Edad: 22}
+	// Bucle tipo while [Valor]
+	n := 0
+	for n < 3 {
+		fmt.Printf("Interacion: %d\n", n)
+		n++
+	}
 
+	// Bucle infinito
+	n = 0
+	for {
+		n++
+		if n == 5 {
+			continue
+		}
 
-	// Imprimir resultados
-	fmt.Println("Suma", suma)
-	fmt.Println("Mensaje", enMayusculas)
-	fmt.Println("Array", arrayFijo)
-	fmt.Println("Slice", sliceVariable)
-	fmt.Println("Mapa", diccionario)
-	fmt.Println("Struct", persona)
-	fmt.Println("Booleano", esVerdadero)
+		fmt.Printf("n en bucle infinito %d\n", n)
+
+		if n >= 7 {
+			break
+		}
+	}
+
+	// Range
+
+	slice := []string{"uno", "dos", "tres"}
+
+	for index, value := range slice {
+		fmt.Printf("Indice: %d, Valor: %s\n", index, value)
+	}
+
+	// Switch
+	valor := 3
+	switch valor {
+	case 1:
+		fmt.Println("Es 1")
+	case 2:
+		fmt.Println("Es 2")
+	default:
+		fmt.Println("No es 1 ni 2")
+	}
 
 
 
 }
-
-//? Tipos
-// bool [true/false]-> falg o condicionales -> false
-// string [Cadena de caracteres]-> representar texto -> ""
-// int, int8, int16, int32, int64 [entero]-> controlar el tamano de los enteros -> 0
-// uint ,uint8,uint16,uint32,uint64 [entero sin signo]-> cuando no queremos negativos ->0
-// float32, float64 [Representar valores numericos reales]-> numeros decimales => 32|64 el sistema -> 0
-// byte === uint8 -> trabajar con datos binarios -> 0
-// rune === int32 -> representar un solo caracter que ocupa mas de un byte 🏆 -> 0
-// complex64, complex12 [Cuando tiene una parte real y otra imaginaria]-> N + iN -> 0 +
