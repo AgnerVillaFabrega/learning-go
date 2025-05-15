8dd1ceafdf70ab2dc548a77c0bce3420cd78aeb3
AgnerVillaFabrega <adejesusvilla@unicesar.edu.co>
2025-05-14 21:58:59 -0500

    Concurrencia & Mutex

--- main.go
+++ main.go
@@ -2,28 +2,71 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"sync"
+	"time"
 )
 
-// Interfaces
-type Forma interface {
-	Area() float64
+// Concurrencia
+
+// go -> GoRoutine
+// GoRoutine -> Hilo de ejecucion ligero virtual
+// Channel -> para comunicar cosas entre GoRoutines
+
+func decirHola(canal chan<- string){
+	time.Sleep(1 * time.Second)
+	canal <- "Hola desde la GoRoutine"
 }
 
-type Circulo struct {
-	Radio float64
-}
-
-func (c Circulo) Area() float64 {
-	return math.Pi * c.Radio * c.Radio
-}
-
-func imprimirArea(f Forma){
-	fmt.Printf("El area es: %.2f\n", f.Area())
+func imprimirMesaje(canal <-chan string){
+	fmt.Println("Esperando el mensaje...")
+	msg := <-canal
+	fmt.Println(msg)
 }
 
 
 func main() {
-	c := Circulo{Radio: 5}
-	imprimirArea(c)
+	canal := make(chan string)
+	go decirHola(canal)
+	imprimirMesaje(canal)
+
+	canal2 := make(chan int)
+	go func(){
+		for i := 0; i<5; i++{
+			canal2 <- i
+		}
+		close(canal2)
+	}()
+
+	for num := range canal2 {
+		fmt.Println("Numero recibido: ", num)
+	}
+
+	// Ejemplo Mutex
+	var contador int
+	var mu sync.RWMutex
+
+	// Writer
+	go func(){
+		for i := 0; i<5; i++{
+			mu.Lock()
+			contador++
+			mu.Unlock()
+			time.Sleep(100 * time.Millisecond)
+		}
+		}()
+
+	// Reader
+	for i:=0; i<3; i++ {
+		go func(id int){
+			for j := 0; j<5; j++{
+				mu.RLock()
+				fmt.Printf("Leyendo desde la GoRoutine %d: %d\n", id, contador)
+				mu.RUnlock()
+				time.Sleep(100 * time.Millisecond)
+			}
+		}(i)
+	}
+
+	time.Sleep(2 * time.Second)
+
 }
