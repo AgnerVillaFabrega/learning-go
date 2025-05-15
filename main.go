814c729c50c05f377478a902ac74d0ef45eda556
AgnerVillaFabrega <adejesusvilla@unicesar.edu.co>
2025-05-14 21:28:27 -0500

    Punteros

--- main.go
+++ main.go
@@ -1,80 +1,30 @@
 package main
 
-import (
-	"errors"
-	"fmt"
-)
+import "fmt"
 
-// Funcion classic
-func suma(a, b int) int{
-	return a + b
+// Punteros
+// * -> puntero
+// & -> refenrencia | Direccion de memoria
+// COPIA LOS ARGUMENTOS
+
+func incrementar(numero *int){
+	// Nunca modifiques el argumento directamente
+	*numero++
 }
 
-// Funcion que devuelve mas de un valor
-func dividir(a, b float64) (float64, error){
-
-	if (b == 0){
-		return 0 , errors.New("no se puede dividir por 0")
-	}
-
-	cociente := a / b
-	// resto := float64(int(a) % int(b))
-
-	return cociente, nil
-}
-
-// Funcion con numero variable de argumentos
-func imprimirNombres(nombres ...string){
-	for _, nombre := range nombres{
-		fmt.Println(nombre)
-	}
-}
-
-// Ejmemplo de closure
-var a = 0
-func contador() func() int{
-	c := 0
-
-	return func() int {
-		c++
-		return c + a
-	}
-}
-
-type Rectangulo struct{
-	Ancho, Alto float64
-}
-
-func(r Rectangulo) Area() float64{
-	return r.Ancho * r.Alto
-}
-
+//? Porque usar punteros?
+// Queremos modificar el elemento original!
+// 
 
 func main() {
-	// cociente, error := dividir(10,0)
-
-	// if error != nil {
-	// 	// comprobaciones
-	// 	fmt.Println(error)
-	// }
-
-	// fmt.Println(cociente)
-
+	valor := 10
+	fmt.Println("Valor antes de incrementar: ", valor)
+	incrementar(&valor)
+	fmt.Println("Valor despues de incrementar: ", valor)
 	
-	// Closure
-	cont := contador()
-	fmt.Println("contador: ", cont())
-	fmt.Println("contador: ", cont())
-	fmt.Println("contador: ", cont())
-	fmt.Println("contador: ", cont())
-	fmt.Println("contador: ", cont())
-	fmt.Println("contador: ", cont())
-	fmt.Println("contador: ", cont())
-	fmt.Println("contador: ", cont())
-	fmt.Println("contador: ", cont())
-	fmt.Println("contador: ", cont())
-	fmt.Println("contador: ", cont())
-
-	rect := Rectangulo{Ancho: 10, Alto: 5}
-	fmt.Println("Area del rectangulo: ", rect.Area())
+	// new()
+	puntero := new(int) //Puntero inicializado en 0
+	fmt.Println("Valor inicial con new: ", *puntero)
+	*puntero = 20
+	fmt.Println("Valor inicial con new: ", *puntero)
 }
