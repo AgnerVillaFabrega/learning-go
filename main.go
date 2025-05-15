7daef2ce98566d13af52f9c009dc8704c2fff2b8
AgnerVillaFabrega <adejesusvilla@unicesar.edu.co>
2025-05-14 21:18:00 -0500

    classic function | function que devuelme mas de un valor| func con numero variable de argumentos | closure | types

--- main.go
+++ main.go
@@ -1,69 +1,80 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
+
+// Funcion classic
+func suma(a, b int) int{
+	return a + b
+}
+
+// Funcion que devuelve mas de un valor
+func dividir(a, b float64) (float64, error){
+
+	if (b == 0){
+		return 0 , errors.New("no se puede dividir por 0")
+	}
+
+	cociente := a / b
+	// resto := float64(int(a) % int(b))
+
+	return cociente, nil
+}
+
+// Funcion con numero variable de argumentos
+func imprimirNombres(nombres ...string){
+	for _, nombre := range nombres{
+		fmt.Println(nombre)
+	}
+}
+
+// Ejmemplo de closure
+var a = 0
+func contador() func() int{
+	c := 0
+
+	return func() int {
+		c++
+		return c + a
+	}
+}
+
+type Rectangulo struct{
+	Ancho, Alto float64
+}
+
+func(r Rectangulo) Area() float64{
+	return r.Ancho * r.Alto
+}
+
 
 func main() {
-	// Defer -> ejecuta cuando termine toda la logica asociada a funcion
-	defer fmt.Println("FIN")
+	// cociente, error := dividir(10,0)
 
+	// if error != nil {
+	// 	// comprobaciones
+	// 	fmt.Println(error)
+	// }
 
-	//  Condicionales
-	// Assertive / negative programming
-	edad := 20
+	// fmt.Println(cociente)
 
-	if edad < 18 {
-		fmt.Println("Eres menor de edad")
-		return
-	}
+	
+	// Closure
+	cont := contador()
+	fmt.Println("contador: ", cont())
+	fmt.Println("contador: ", cont())
+	fmt.Println("contador: ", cont())
+	fmt.Println("contador: ", cont())
+	fmt.Println("contador: ", cont())
+	fmt.Println("contador: ", cont())
+	fmt.Println("contador: ", cont())
+	fmt.Println("contador: ", cont())
+	fmt.Println("contador: ", cont())
+	fmt.Println("contador: ", cont())
+	fmt.Println("contador: ", cont())
 
-	fmt.Println("Eres mayor de edad")
-
-	// Bucle [Rango]
-	for i := 0; i < 5; i++ {
-		fmt.Printf("Iteracion: %d\n", i)
-	}
-
-	// Bucle tipo while [Valor]
-	n := 0
-	for n < 3 {
-		fmt.Printf("Interacion: %d\n", n)
-		n++
-	}
-
-	// Bucle infinito
-	n = 0
-	for {
-		n++
-		if n == 5 {
-			continue
-		}
-
-		fmt.Printf("n en bucle infinito %d\n", n)
-
-		if n >= 7 {
-			break
-		}
-	}
-
-	// Range
-
-	slice := []string{"uno", "dos", "tres"}
-
-	for index, value := range slice {
-		fmt.Printf("Indice: %d, Valor: %s\n", index, value)
-	}
-
-	// Switch
-	valor := 3
-	switch valor {
-	case 1:
-		fmt.Println("Es 1")
-	case 2:
-		fmt.Println("Es 2")
-	default:
-		fmt.Println("No es 1 ni 2")
-	}
-
-
-
+	rect := Rectangulo{Ancho: 10, Alto: 5}
+	fmt.Println("Area del rectangulo: ", rect.Area())
 }
